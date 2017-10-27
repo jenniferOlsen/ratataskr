84670f25668363f07dc6699481025e7055897ae7
Jenny Olsen <jenny.a.olsen@gmail.com>
2017-10-27 11:20:57 -0700

    get user information on frontend

--- app/app.js
+++ app/app.js
@@ -19,18 +19,35 @@ const app = feathers()
    }));
 const mapsService = app.service('/maps');
 const usersService = app.service('/users');
+let currentUser = '';
 
 app.authenticate()
  .then(response => {
    console.info('Feathers Client has Authenticated with the JWT access token!');
    console.log(response);
+   return app.passport.verifyJWT(response.accessToken);
  })
+ .then(payload => {
+   console.log('payload', payload)
+   return app.service('users').get(payload.userId);
+ })
+ .then(user => {
+  app.set('user', user);
+  console.log('User', app.get('user'));
+  currentUser = app.get('user');
+})
  .catch(error => {
    console.info('We have not logged in with OAuth, yet.  This means there\'s no cookie storing the accessToken.  As a result, feathersClient.authenticate() failed.');
    console.log(error);
 });
 
 class App extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    name: currentUser.name
+  };
+}
 
   newMap() {
     mapsService.create({ title: 'New Test 2', coordinatesRange: [0,0] }).then(function(response) {
@@ -38,7 +55,12 @@ class App extends Component {
     })
   }
 
+
+
   render() {
+    console.log('state ', this.state.name);
+    // console.log('name', user);
+    console.log('curent', currentUser)
 
     return (
       <div>
@@ -46,6 +68,7 @@ class App extends Component {
         <p><br/>
           <a className="button" href="/auth/google">Login With Google</a>
         </p>
+        <p>User: {this.state.user}</p>
         <p><a onClick={this.newMap}>Add new map</a></p>
       </div>
     );
