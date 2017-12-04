3a6928b01c5b62c6bc3823c7d7e6c0ae6aa51dca
Jenny Olsen <jenny.a.olsen@gmail.com>
2017-12-04 11:28:09 -0800

    form to set map title

--- app/components/AddMap.js
+++ app/components/AddMap.js
@@ -5,11 +5,22 @@ import '../styles.scss';
 class AddMap extends Component {
   constructor(props) {
   super(props);
-  this.state = { };
+  this.state = {
+    mapTitle: '',
+  };
 }
 
-  newMap(){
-    this.props.socket.emit('maps::create', {title:'New from Component', coordinatesRange: [0,0] }, (error, data) => {
+  handleInput(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({[name]: value});
+  }
+
+  newMap(event){
+    event.preventDefault();
+    let title = this.state.mapTitle;
+    this.props.socket.emit('maps::create', {title: title, coordinatesRange: [0,0] }, (error, data) => {
       console.log('new map', data)
       if (error) {
         console.error(error)
@@ -19,7 +30,13 @@ class AddMap extends Component {
 
   render() {
     return (
-      <a onClick={this.newMap.bind(this)}>Add new map</a>
+      <form onSubmit={this.newMap.bind(this)}>
+        <label>
+          Map title:
+          <input type='text' name='mapTitle' value={this.state.value} onChange={this.handleInput.bind(this)} />
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
     );
   }
 }
